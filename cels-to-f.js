d0ce83186b2bf8175160733cfcf90bb243574e5c
=Raunak <=sharmaraunak338@gmail.com>
2019-04-19 21:52:11 +0530

    coverting celsius to fahrenheit

---
+++ cels-to-f.js
@@ -0,0 +1,10 @@
+//covering celcius to  fahrenheit
+/**
+ *
+ * @param {*} cel value of temperature in celcius
+ * fahrenheit = 9*cel/5 + 32
+ */
+function convertToF(cel) {
+  return (9 * cel) / 5 + 32;
+}
+console.log(convertToF(0));
